6983f8dfa8b0742ba938f8bc6ab0e629f8d18964
Florian Metzger-Noel <florian.metzger-noel@ml-pa.com>
2025-05-23 10:12:07 +0200

    fix: column functions not found

--- fins/dsl/command_column.py
+++ fins/dsl/command_column.py
@@ -48,5 +48,4 @@ class ColumnCommand(Command):
 
 """Register all column classes as commands."""
 for col_class in Column.list():
-    name = str(col_class())  # Get default name from instance
-    Command.register_command(name, ColumnCommand)  # Register the class, not an instance
+    Command.register_command(f"function_{col_class()}", ColumnCommand)  # Register the class, not an instance
