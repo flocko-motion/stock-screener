b2008a455db318627c38c68c0a71836986c10459
Florian Metzger-Noel <florian.metzger-noel@ml-pa.com>
2025-06-22 15:27:11 +0200

    feat: complete restart ... now with Python instead of a DSL

--- fins/tests/test_basket.py
+++ fins/tests/test_basket.py
@@ -63,45 +63,6 @@ class BasketTests(unittest.TestCase):
 
 
 
-    def test_simple_basket(self):
-        basket = Basket(name="foo", items=[
-            BasketItem("AAPL"),
-        ])
-
-        cols = [
-            columns.McapColumn(),
-            columns.NpmColumn(),
-            columns.PeColumn(),
-            columns.PegColumn(),
-            columns.RoeColumn(),
-            columns.VolColumn(),
-            columns.YieldColumn(),
-        ]
-        for col in cols:
-            basket._add_column(col)
-
-        desc = basket.to_dict()
-
-        assert not (desc is None)
-        assert desc["name"] == "foo"
-        assert len(desc["items"]) == 1
-        assert desc["items"][0]["ticker"] == "AAPL"
-        assert desc["items"][0]["amount"] == 1
-        assert desc["items"][0]["class"] == "BasketItem"
-
-        assert len(desc["columns"]) == len(cols)
-        assert desc["columns"][0]["class"] == "McapColumn"
-        assert desc["columns"][1]["class"] == "NpmColumn"
-        assert desc["columns"][2]["class"] == "PeColumn"
-        assert desc["columns"][3]["class"] == "PegColumn"
-        assert desc["columns"][4]["class"] == "RoeColumn"
-        assert desc["columns"][5]["class"] == "VolColumn"
-        assert desc["columns"][6]["class"] == "YieldColumn"
-
-        data = basket.df()
-        assert not (data is None)
-        print(f"\n{data}\n")
-
     def assertBasket(self, basket: Basket, expected: dict):
         """
         Assert that the basket contains exactly the expected items with expected weights.
@@ -128,22 +89,22 @@ class BasketTests(unittest.TestCase):
             self.assertIn(ticker, expected, f"Unexpected ticker {ticker} found in basket")
 
 
-    @time_it
-    @no_cache
-    @unbuffered_output
-    def test_multithreaded_basket_creation_one(self):
-        symbols = ["AAPL"]
-        basket = Basket.from_symbols(symbols)
-        assert len(basket._items) == len(symbols)
-
-    @time_it
-    @no_cache
-    @unbuffered_output
-    def test_multithreaded_basket_creation(self):
-        symbols = ["ATLFF","ALFNF","ACMDY","ASBPW","CWBR","ATIW","FBTC","FXED","PFS","DGRS","BUFIX","GPFT","FFTI","ASXSF","BKAYY","AMH-PE","RGNX","WSCC","RXST","ACU","TIBGX","ZSPY","AHG","GOP","FTA","HROWM","FORD","RSYEX","NNAVW","HNGZY","TNGRF","MARUY","HIZOF","ALRY","RUSHA","ORMP","WLGS","SRTS","DISSX","ULNV","SOGFF","DFLIW","TCEFF","APPZ","CGO","YAMCF","NIKLF",]
-        # symbols = symbols[0:10]
-        basket = Basket.from_symbols(symbols, max_workers=50)
-        assert len(basket._items) == len(symbols)
+    # @time_it
+    # @no_cache
+    # @unbuffered_output
+    # def test_multithreaded_basket_creation_one(self):
+    #     symbols = ["AAPL"]
+    #     basket = Basket.from_symbols(symbols)
+    #     assert len(basket._items) == len(symbols)
+    #
+    # @time_it
+    # @no_cache
+    # @unbuffered_output
+    # def test_multithreaded_basket_creation(self):
+    #     symbols = ["ATLFF","ALFNF","ACMDY","ASBPW","CWBR","ATIW","FBTC","FXED","PFS","DGRS","BUFIX","GPFT","FFTI","ASXSF","BKAYY","AMH-PE","RGNX","WSCC","RXST","ACU","TIBGX","ZSPY","AHG","GOP","FTA","HROWM","FORD","RSYEX","NNAVW","HNGZY","TNGRF","MARUY","HIZOF","ALRY","RUSHA","ORMP","WLGS","SRTS","DISSX","ULNV","SOGFF","DFLIW","TCEFF","APPZ","CGO","YAMCF","NIKLF",]
+    #     # symbols = symbols[0:10]
+    #     basket = Basket.from_symbols(symbols, max_workers=50)
+    #     assert len(basket._items) == len(symbols)
 
 
 if __name__ == "__main__":
